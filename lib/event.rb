0a08cc3ccc6bb2f84cbab1959634e3873e835891
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-15 14:51:09 -0600

    Adds final commit, WIP on total inventory

--- lib/event.rb
+++ lib/event.rb
@@ -20,4 +20,27 @@ class Event
       food_truck.inventory.include?(item)
     end
   end
+
+  def quantity_of_items
+    item_count = Hash.new(0)
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item|
+        item_count[:quantity] += item[1] if item == item
+      end
+    end
+    # require "pry"; binding.pry
+  end
+
+  # def total_inventory
+  #   available_inventory = Hash.new {|h, k| h[k] = {} }
+  #   quantity = Hash.new
+  #   @food_trucks.each do |food_truck|
+  #     food_truck.inventory.each do |item|
+  #       available_inventory[item] = food_trucks_that_sell(item)
+  #       # available_inventory[item] = quantity[item[-1]]
+  #       require "pry"; binding.pry
+  #     end
+  #   end
+  #   available_inventory
+  # end
 end
