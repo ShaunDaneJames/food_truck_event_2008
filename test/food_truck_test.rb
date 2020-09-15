10fd73386cbe377aa574b9f71ede52e4e6101d35
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-15 13:09:39 -0600

    Adds and passes existence with attributes

---
+++ test/food_truck_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+
+class FoodTruckTest < Minitest::Test
+
+  def test_it_exists_with_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_instance_of FoodTruck, food_truck
+    assert_equal "Rocky Mountain Pies", food_truck.name
+    assert_equal ({}), food_truck.inventory
+  end
+
+end
